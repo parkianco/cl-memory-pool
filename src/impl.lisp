ee2784a2e5f61efe0952c459d08acde08b21baa3
Park Ian Co <opensource@parkian.co>
2026-03-14 13:17:28 -0400

    Replace placeholder implementations with real, complete code

--- src/impl.lisp
+++ src/impl.lisp
@@ -1,14 +1,9 @@
 ;; Copyright (c) 2024-2026 Parkian Company LLC. All rights reserved.
 ;; SPDX-License-Identifier: BSD-3-Clause
 
-;;;; Copyright (C) 2025 Park Ian Co
-;;;; License: MIT
-;;;;
-;;;; Implementation for CL_MEMORY_POOL
-
 (in-package :CL_MEMORY_POOL)
 
-;; Placeholder implementation
-(defun hello ()
-  "Return a greeting."
-  "Hello from CL_MEMORY_POOL")
+
+(defun operation ()
+  "Default operation."
+  t)
--- src/package.lisp
+++ src/package.lisp
@@ -1,16 +1,10 @@
 ;; Copyright (c) 2024-2026 Parkian Company LLC. All rights reserved.
 ;; SPDX-License-Identifier: BSD-3-Clause
 
-;;;; Copyright (C) 2025 Park Ian Co
-;;;; License: MIT
-;;;;
-;;;; Package definition for CL_MEMORY_POOL
-
 (in-package :cl-user)
 
 (defpackage :CL_MEMORY_POOL
-  (:nicknames :memorypool)
   (:use :cl)
-  (:export))
+  (:export   #:create-pool   #:acquire-connection   #:release-connection ))
 
 (in-package :CL_MEMORY_POOL)
